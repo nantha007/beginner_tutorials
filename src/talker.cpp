50cbbcdcb1c7870586f5557c4005ef5dd5b20fd2
nantha007 <nantha07@terpmail.umd.edu>
2018-10-30 20:35:01 -0400

    Adding the script to publish messages on topic chatter

---
+++ src/talker.cpp
@@ -0,0 +1,56 @@
+/**
+ * MIT License
+ * Copyright (c) 2018 Nantha Kumar Sunder
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+/**
+ *  @file talker.cpp
+ *  @brief A program to create a Node to publish messages on topic chatter
+ *  @author Nantha Kumar Sunder
+ *  @copyright 2018
+ */
+#include <sstream>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "talker");
+  ros::NodeHandle n;
+  ros::Publisher chatter_pub = n.advertise < std_msgs::String
+      > ("chatter", 1000);
+  ros::Rate loop_rate(10);
+  /**
+   * The loop send the string "ROS tutorials" 
+   * to the channel chatter along with the count 
+   * of the loop iteration
+   */
+  int count = 0;
+  while (ros::ok()) {
+    std_msgs::String msg;
+    std::stringstream ss;
+    ss << "ROS tutorial" << count;
+    msg.data = ss.str();
+    ROS_INFO("%s", msg.data.c_str());
+    chatter_pub.publish(msg);
+    ros::spinOnce();
+    loop_rate.sleep();
+    ++count;
+  }
+  return 0;
+}
